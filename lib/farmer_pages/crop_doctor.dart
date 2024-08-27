7e6831a292536801d569b921858b110250ba7464
Karthik <karthikchallagundla18@gmail.com>
2024-08-27 15:40:10 +0530

    Updated Voice Chat

--- lib/farmer_pages/crop_doctor.dart
+++ lib/farmer_pages/crop_doctor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class CropDoctor extends StatefulWidget {
   const CropDoctor({super.key});
@@ -8,9 +9,54 @@ class CropDoctor extends StatefulWidget {
 }
 
 class _CropDoctorState extends State<CropDoctor> {
+  final SpeechToText speechToText = SpeechToText();
+  bool speechEnabled = false;
+  bool isListening = false;
+  TextEditingController wordSpoken = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeech();
+  }
+
+  void initSpeech() async {
+    speechEnabled = await speechToText.initialize();
+    setState(() {});
+  }
+
+  void onSpeechResult(result) {
+    setState(() {
+      wordSpoken.text = result.recognizedWords;
+    });
+  }
+
+  void startListening() async {
+    if (speechEnabled && !isListening) {
+      await speechToText.listen(
+        onResult: onSpeechResult,
+        listenOptions: SpeechListenOptions(
+          listenMode: ListenMode.dictation,
+        )
+      );
+      print(wordSpoken.text);
+      setState(() {
+        isListening = true;
+      });
+    }
+  }
+
+  void stopListening() async {
+    if (isListening) {
+      await speechToText.stop();
+      setState(() {
+        isListening = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
     List<Map<String, dynamic>> doctorsData = [
       {
         'name': 'Mohanji',
@@ -73,8 +119,12 @@ class _CropDoctorState extends State<CropDoctor> {
                         children: [
                           CircleAvatar(
                             radius: 40,
-                            backgroundImage: (doctorsData[index]['imgUrl'].toString().isNotEmpty) ? NetworkImage(doctorsData[index]['imgUrl']) : null,
-                            child: (doctorsData[index]['imgUrl'].toString().isEmpty) ? Text('No Image', style: TextStyle(fontSize: 12),) : null,
+                            backgroundImage: (doctorsData[index]['imgUrl'].toString().isNotEmpty)
+                                ? NetworkImage(doctorsData[index]['imgUrl'])
+                                : null,
+                            child: (doctorsData[index]['imgUrl'].toString().isEmpty)
+                                ? Text('No Image', style: TextStyle(fontSize: 12))
+                                : null,
                           ),
                           SizedBox(width: 15),
                           Expanded(
@@ -107,17 +157,27 @@ class _CropDoctorState extends State<CropDoctor> {
                   child: SizedBox(
                     height: 50,
                     child: TextField(
+                      controller: wordSpoken,
                       style: TextStyle(fontSize: 16),
                       textAlignVertical: TextAlignVertical.center,
-                      decoration: InputDecoration(hintText: 'Enter your problem', border: OutlineInputBorder(borderRadius: BorderRadius.circular(50))),
+                      decoration: InputDecoration(
+                        hintText: 'Enter your problem',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: isListening ? stopListening : startListening,
+                          icon: isListening ? Icon(Icons.mic_off) : Icon(Icons.mic),
+                        ),
+                      ),
                     ),
                   ),
                 ),
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {},
                   style: ElevatedButton.styleFrom(shape: CircleBorder()),
                   child: Icon(Icons.arrow_upward),
-                )
+                ),
               ],
             ),
             SizedBox(height: 20,),
