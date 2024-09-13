c51265514073ffb7a71c6682f3dc5e0bc9efda7e
Karthik <karthikchallagundla18@gmail.com>
2024-09-13 14:23:24 +0530

    Updated schemes data

---
+++ lib/database/scheme_data.dart
@@ -0,0 +1,126 @@
+List<Map<String, dynamic>> schemeData = [
+  {
+    "Name":"Pradhan Mantri Kisan Maandaan Yojana",
+    "Type": "Central",
+    "Shortcut": "PMKMY",
+    "Purpose": "To support small and marginal farmers in India that provides a monthly pension in old age",
+    "Eligibility": "Farmers between  of 18to 40 who own cultivable land up to 2 hectares are eligible",
+    "Benefits":" Once farmers reach age of 60, they will receive  monthly pension of Rs.3000",
+    "Enrollment":"Farmers an enroll online or at Common Service Centres",
+    "Family pension": "If the eligible subscriber dies while receiving a pension, their spouse is entitled to 50% f the pension as a family pension",
+    "Contributions": "Farmers will contribute between Rs.50 and Rs.200 per month, while government contributes an equal amount"
+  },
+  {
+    "Name": "National Mission for Agriculture Extension Technology" ,
+    "Type": "Central",
+    "Shortcut":" NMAET",
+    "Purpose": "Distribution of Agriculture Equipment under the sub mission on agriculture mechanisation",
+    "Eligibity": "Farmers in the rural of regional areas having forest, plateau and semi urban lands", 
+    "Crops": "All crops",
+    "Benefits": "Government will help in increase farm power availability and reduce costs incurred in agriculture works", 
+    "Verification": "District level contractual appointment, Deputy project director"
+  },
+  {
+    "Name": "National Mission for Substainable Agriculture (Swayel Health Card Scheme)",
+    "Type": "Central",
+    "Shortcut": "NMSA-I",
+    "Purpose": "Encourage Balanced and Integrated Fertilizer use in health card in 3 years", 
+    "Eligibility": "soil pH level should be in fixed range at a particular region with nutrition management", 
+    "Benefits": "Government will provide farmers in establishing of soil testing laboratories and support harmless fertilizer with subsidy", 
+    "Crops": "All crops", 
+    "Verification": "A team of agriculture management system with leading district agriculture officer"
+  },
+  {
+    "Name" : "Prime Minister Agriculture Irrigation Scheme (Integrated Water up take Management)",
+    "Type": "Central",
+    "Shortcut": "PMAIS",
+    "Purpose": "Effective Management of soil and water conservation in the catchment area", 
+    "Reason": "Insufficient water sources nearby and poor water sources available", 
+    "Crops": "For all crops which facing less irrigation management", 
+    "Benefits": "Government will construct water capacity buildings in effective rain trap management, drainage line treatment and soil and moisture management", 
+    "Situation": "Farmers are facing lack of irrigation in the cultivation in the unit", 
+    "Verification": "Water Authority project officer, water supply committee chairman"
+  },
+
+  {
+    "Name" :"Prime Minister Crop Insurance Scheme",
+    "Type": "Central",
+    "Shortcut":"PMCIS",
+    "Purpose" : "Empowering farmers in natural calamities, pests and destruction, providing insurance and financial assistance to farmers", 
+    "Eligibility": "Farmers should be notified having crop loan account and crop credit limit as been approved during the season", 
+    "Crops" : "Rice , Soyabean, Peanut, Wheat" ,
+    "Benefits": "Providing financial assistance of 90% of the damage supported by companies Agriculture Insurence of India, ICICI , HDFC, Tata Agriculture", 
+    "Situation": "Crop should damage 80%-90% of entire cultivated land in the unit",
+    "Verification" : "District level monitoring committee under the chairman ship of District Collector"
+  },
+  {
+    "Name": "National Mission for Sustainable Agriculture (Rain fed area development plan)",
+    "Type": "Central",
+    "Shortcut": "NMSA-II",
+    "Theme": "providing livelihood resources to the farmers in rain fed areas by reduction of risks through integrated agriculture system.",
+    "Eligibility": "farmers should notify and enrolled their details of the crop in the nearby Mandal agriculture center. ",
+    "Crops": "all crops are eligible", 
+    "Benefits": "Government will provide an amount of 1 lakh excluding cost of construction of ponds, repairs, shortage of polyhouses.",
+    "Situation": "farmers affecting in heavy rainfall in rain fed area can avail this scheme.",
+    "Verification": "a team of agriculture management system with leading district agriculture officer."
+  },
+  {
+    "Name": "Prime Minister Agriculture Irrigation Scheme{per drop more crop}",
+    "Type": "Central",
+    "Shortcut":" PMAIS",
+    "Purpose": "Affective management of soil and water conservation to increase the irrigation efficiency.",
+    "Reason": "Insufficient water sources nearby, poor, water sources available.",
+    "Benefits": "Government will provide 60% subsidy for the sprinkler and drip irrigation system for poor farmers and 40% subsidy for moderate farmers.",
+    "Situation":" Farmers are facing lack of irrigation in the cultivation in an unit.",
+    "Verification":" Water authority project officer water supply committee chairman and Regional Rural Agricultural Extension officer."
+  },
+  {
+    "Name":" National Mission for Sustainable Agriculture{the traditional agriculture development plan}",
+    "Type": "Central",
+    "Shortcut": "NMSA-III",
+    "Theme":" organic farming by adopting low cast environmental friendly techniques.",
+    "Eligibility": "Organic-the respected crop should practice chemical less farming from an year past.",
+    "Crops": "All crops are eligible",
+    "Benefits":" Government will support organic chemicals & fertilizers and promote certified organic farming based on participatory guarantee system ",
+    "Situation": "who are willing for organic farming.",
+    "Verification": "soil testing report verified through agriculture portal."
+  },
+  {
+    "Name": "National Mission for Agriculture Extension Technology",
+    "Type": "Central",
+    "Shortcut": "NMAET",
+    "Theme": "All mission on agriculture extension.",
+    "Eligibility": "Farmers in the rural of regional areas having forests, plateau lands ,semi urban lands.",
+    "Crops": "All crops are eligible.",
+    "Benefits": "Government will provide financial and subsidize the equipment for all classes of farmers.",
+    "Verification" :"district level contractual appointment deputy project director."
+  },
+  {
+    "Name": "Andhra Pradesh Micro Irrigation Project",
+    "Type": "State",
+    "Shortcut": "APMIP",
+    "Purpose": "To enhance and support small and marginal farmers for cultivation of crop based on their income",
+    "Eligibility": "100% subsidy for SC/ST farmers with land less than 5 acres and income up to 2 lakhs,90% subsidy for other farmers with land less than 5 acres and income up to 2 lakhs, 70% subsidy for medium farmers with land between 5 and 10 acres and income up to 2.8 lakhs, 50% subsidy for all other farmers with land more than 10 acres", 
+    "Benefits": "equipment for drip irrigation are provided under subsidy",
+    "Reason": "To reduce power consumption, decrease pest and disease attack, to save irrigation water and increase 30% yields"
+  },
+  {
+    "Name": "Kisan No Interest Loans on Crop Cultivation (Sunna Vaddi Panta Runalu)",
+    "Type": "State",
+    "Shortcut": "KILCC",
+    "Purpose": "To ensure enhanced production of crops, besides providing crop loans to farmers at a cheaper interest rate",
+    "Reason": "receiving high number of suicides by Farmers of Telugu states due to loans with high interest and insufficient profits and high losses Reducing the debt burden of small and marginal farmers and encouraging repayment",
+    "Eligibility": "Beneficiaries under the scheme are farmers who have taken loans from banks and are prompt in repayment of their loans. The farmers are required to repay their outstanding loans within  due date",
+    "Benefits": "Farmers can get loan up to Rs.1 lakh with interest 1% and return the loan within the due date",
+    "Implementing Agencies" : "Department of Agriculture and Coperation, Government of Andhra Pradesh through banks"
+  },
+  {
+    "Name": "Andhra Pradesh Rythu Raksha Scheme",
+    "Type": "State",
+    "Shortcut": "APRR",
+    "Purpose": "To provide financial assistance to farmers in the state by amount Rs. 5000 is given to farmers by starting of Kharif and Rabi seasons to prepare land, purchasing and sowing seeds and to pay labour charges",
+    "Objective": "To provide financial security in both sowing seasons and stop farmer suicides in taking loans",
+    "Benefits": "Financial assistance of Rs. 10000 per year, and given at starting of sowing period",
+    "Eligibility": "All small and marginal farmers of Andhra Pradesh all eligible",
+  }
+];
--- lib/farmer_pages/schemes.dart
+++ lib/farmer_pages/schemes.dart
@@ -1,3 +1,4 @@
+import 'package:demo_mvp/database/scheme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:translator/translator.dart';
 
@@ -16,27 +17,7 @@ class _SchemesState extends State<Schemes> {
   @override
   Widget build(BuildContext context) {
 
-    List<Map<String, dynamic>> schemesData = [
-      {
-        "NAME": "National mission for sustainable agriculture (Rain fed area development plan)",
-        "Scheme": "Central",
-        "Theme": "Providing livelihood resources to the farmers in rain fed areas by reduction of risks through integrated agriculture system.",
-        "Eligibility": "Farmers should notify and enrolled their details of the crop in the nearby mandal agriculture center.",
-        "Crops": "All crops are eligible",
-        "Benefits": "Government will provide an amount of 1 lakh excluding cost of construction of ponds, repairs,shortage of polyhouses.",
-        "Situation": "Farmers affecting in heavy rainfall in rain fed area can avail this scheme.",
-        "Verification": "A team of agriculture management system with leading district agriculture officer.",
-      },
-      {
-        "NAME": "National Mission for Substantial Agriculture (Swayel Health Card Scheme)",
-        "SCHEME": "Central",
-        "THEME": "Encourage Balaced and Integrated Fertilizer use in health card in 3 years",
-        "ELIGIBILITY": "Soil pH level should be in fixed range at a particular region with nutrition management",
-        "BENEFITS": "Government will provide farmers in establishing of soil testing laboratories and support harmless fertilizer with subsidy",
-        "CROPS": "All crops", 
-        "VERIFICATION": "A team of agriculture management system with leading district agriculture officer"
-      },
-    ];
+    List<Map<String, dynamic>> schemesData = schemeData;
 
     return Scaffold(
       appBar: AppBar(
@@ -74,14 +55,14 @@ class _SchemesState extends State<Schemes> {
               itemCount: schemesData.length,
               itemBuilder: (context, index) {
                 Map<String, dynamic> scheme = schemesData[index];
-                return Padding(
+                return !((central && scheme['Type'] == 'Central') || (state && scheme['Type'] == 'State')) ? Container() : Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: Card(
                     child: ListTile(
                       onTap: () {
                         Navigator.of(context).push(MaterialPageRoute(builder: (context) => SchemeDetails(scheme: scheme)));
                       },
-                      title: Text(scheme['NAME'], style: TextStyle(fontWeight: FontWeight.bold),),
+                      title: Text(scheme['Name'], style: TextStyle(fontWeight: FontWeight.bold),),
                       trailing: Icon(Icons.arrow_forward_ios_rounded),
                     ),
                   ),
@@ -114,12 +95,11 @@ class _SchemeDetailsState extends State<SchemeDetails> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(0, 200, 0, 0.8),
-        title: Text('${scheme['NAME']} Details'),
+        title: Text('${scheme['Shortcut']} Details'),
       ),
       body: Container(
-        decoration: BoxDecoration(color: Colors.orange),
         child: ListView.builder(
-          itemCount: keys.length - 2,
+          itemCount: keys.length - 3,
           itemBuilder: (context, index) {
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -129,16 +109,16 @@ class _SchemeDetailsState extends State<SchemeDetails> {
                   decoration: BoxDecoration(
                     color: Colors.blue
                   ),
-                  child: Text(keys[index + 2].toUpperCase(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white), textAlign: TextAlign.center,)
+                  child: Text(keys[index + 3].toUpperCase(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white), textAlign: TextAlign.center,)
                 ),
                 SizedBox(height: 10,),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: FutureBuilder<String>(
-                    future: translateText(scheme[keys[index + 2]], currentLocale.languageCode),
+                    future: translateText(scheme[keys[index + 3]], currentLocale.languageCode),
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Text('');
+                        return Center(child: CircularProgressIndicator(),);
                       } else if (snapshot.hasError) {
                         return Text(scheme[keys[index + 1]]);
                       } else {
